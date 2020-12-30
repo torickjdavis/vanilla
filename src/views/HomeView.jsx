7af044260b8cef0fc2dda65fe05c7ef73a8b4331
Torick J Davis <torickjdavis@gmail.com>
2020-12-29 18:16:33 -0700

    Abstract ViewportGrid for Reuse in Views
    The ViewportGrid displays the children content above the fold.
    Additional content after the fold can be added as well, and then is
    visible only after scrolling.

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -11,29 +11,9 @@ import { Search as SearchIcon } from '@material-ui/icons';
 import clsx from 'clsx';
 import { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import ViewportGrid from '../components/ViewportGrid';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    position: 'relative',
-    background: theme.palette.primary.main,
-    '&::before': {
-      content: '""',
-      position: 'absolute',
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      background: `url("/assets/andy-chilton-0JFveX0c778-unsplash.jpg")`,
-      backgroundSize: 'cover',
-      backgroundAttachment: 'fixed',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      opacity: 0.5,
-    },
-  },
-  hero: {
-    maxHeight: '100vh',
-  },
   heroText: {
     zIndex: 1,
     position: 'relative',
@@ -55,38 +35,26 @@ export default function HomeView() {
   if (redirect) return <Redirect to={redirect} />;
 
   return (
-    <Grid container className={clsx(classes.root, 'fill-height')}>
-      <Grid
-        container
-        justify="center"
-        alignItems="center"
-        direction="column"
-        item
-        className={clsx(classes.hero, 'fill-height')}
-      >
-        <Grid item>
-          <Container className={classes.heroText}>
-            <Typography variant="h1">Vanilla</Typography>
-            <Typography variant="h3">
-              Create, collect, and share recipes.
-            </Typography>
-            <Button
-              variant="contained"
-              size="large"
-              color="secondary"
-              className={classes.callToAction}
-              startIcon={<SearchIcon />}
-              onClick={() => setRedirect('/explore')}
-            >
-              Explore Recipes
-            </Button>
-          </Container>
-        </Grid>
-        <Grid item className={classes.callToAction}></Grid>
+    <ViewportGrid backgroundImageURL="/assets/andy-chilton-0JFveX0c778-unsplash.jpg">
+      <Grid item>
+        <Container className={classes.heroText}>
+          <Typography variant="h1">Vanilla</Typography>
+          <Typography variant="h3">
+            Create, collect, and share recipes.
+          </Typography>
+          <Button
+            variant="contained"
+            size="large"
+            color="secondary"
+            className={classes.callToAction}
+            startIcon={<SearchIcon />}
+            onClick={() => setRedirect('/explore')}
+          >
+            Explore Recipes
+          </Button>
+        </Container>
       </Grid>
-      {/* <Grid container item>
-        Additional content below the fold.
-      </Grid> */}
-    </Grid>
+      <Grid item className={classes.callToAction}></Grid>
+    </ViewportGrid>
   );
 }
