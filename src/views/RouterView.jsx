883a9bb75fa9d6f657792bda048fa73da70a8549
Torick J Davis <torickjdavis@gmail.com>
2021-01-10 07:46:30 -0700

    Create Custom Router Hooks and Use useRouteMatch
    The custom route hooks wrap the React Router hooks with similar names.
    They were created because the provided hooks don't appear to work with
    the dialog system I've created.
    This is something I should address in order to better use the pre-built
    hooks and potentially simplify code.

--- src/views/RouterView.jsx
+++ src/views/RouterView.jsx
@@ -1,16 +1,28 @@
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation, useRouteMatch } from 'react-router-dom';
 import GuardRoute from '../components/GuardRoute';
 import { useAuth } from '../contexts/AuthContext';
 import ErrorView from './ErrorView';
+
+// RouterView hooks used to simulate regular React Router hooks, but in a Dialog
+
+export const useRouterViewMatch = (routes) => {
+  return useRouteMatch({ path: routes.map((r) => r.path), exact: true });
+};
+
+export const useRouterViewParams = (routes) => {
+  const { params } = useRouterViewMatch(routes);
+  return params;
+};
 
 export default function RouterView({ routes }) {
   const { isAuthenticated } = useAuth();
   const location = useLocation();
   const backdrop = location.state?.backdrop;
 
-  const findRoute = (path) => routes.find((r) => r.path === path);
+  const match = useRouteMatch({ path: routes.map((r) => r.path), exact: true });
 
-  const modal = backdrop && findRoute(location.pathname)?.component;
+  // prettier-ignore
+  const modal = backdrop && routes.find((r) => r.path === match.path)?.component;
 
   const text = [
     'Route',
