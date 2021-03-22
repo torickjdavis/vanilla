7cfcb7f1b459d92b5b1f0353bfdbaf52ba679511
Torick J Davis <torickjdavis@gmail.com>
2021-03-22 16:05:14 -0600

    Modify Resource for Functional Approach
    This simplifies the resource routes by adding a wrapper.

--- server/src/resource.js
+++ server/src/resource.js
@@ -2,128 +2,118 @@ import { Router } from 'express';
 // import status from 'statuses';
 import status from 'http-status';
 
-export class ModelController {
-  constructor(model) {
-    this.model = model;
-    this.name = this.model.modelName;
-    this.collection = this.model.collection.name;
+// Create
+const create = ({ model }) => async (req, res, next) => {
+  // use body and let schema validation handle keys
+  try {
+    const instance = new model(req.body);
+    await instance.save();
+    res.status(status.CREATED).json(instance);
+  } catch (error) {
+    next(error);
   }
+};
 
-  // Create
-  async create(req, res, next) {
-    // use body and let schema validation handle keys
-    try {
-      console.debug(`Create ${this.name}`);
-      const instance = new this.model(req.body);
-      await instance.save();
-      res.status(status.CREATED).json(instance);
-    } catch (error) {
-      next(error);
+// Read
+const read = ({ model, name }) => async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const instance = await model.findById(id).exec();
+    if (instance) res.json(instance);
+    else {
+      res
+        .status(status.NOT_FOUND)
+        .json({ message: `No ${name} Found (${id})` });
     }
+  } catch (error) {
+    next(error);
   }
+};
 
-  // Read
-  async read(req, res, next) {
-    try {
-      console.debug(`Read ${this.name}`);
-      const id = req.params.id;
-      const instance = await this.model.findById(id).exec();
-      if (instance) res.json(instance);
-      else {
-        res
-          .status(status.NOT_FOUND)
-          .json({ message: `No ${this.name} Found (${id})` });
-      }
-    } catch (error) {
-      next(error);
-    }
+// Update
+const replace = ({ model, name }) => async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    await model.findOneAndReplace({ _id: id }, req.body).exec();
+    res.json({
+      success: true,
+      message: `Replaced ${name} Instance (${id})`,
+    });
+  } catch (error) {
+    next(error);
   }
+};
 
-  // Update
-  async replace(req, res, next) {
-    try {
-      console.debug(`Replace ${this.name}`);
-      const id = req.params.id;
-      await this.model.findOneAndReplace({ _id: id }, req.body).exec();
-      res.json({
-        success: true,
-        message: `Replaced ${this.name} Instance (${id})`,
-      });
-    } catch (error) {
-      next(error);
-    }
+const modify = ({ model, name }) => async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    await model.findByIdAndUpdate(id, req.body).exec();
+    res.json({
+      success: true,
+      message: `Modified ${name} Instance (${id})`,
+    });
+  } catch (error) {
+    next(error);
   }
+};
 
-  async modify(req, res, next) {
-    try {
-      console.debug(`Modify ${this.name}`);
-      const id = req.params.id;
-      await this.model.findByIdAndUpdate(id, req.body).exec();
-      res.json({
-        success: true,
-        message: `Modified ${this.name} Instance (${id})`,
-      });
-    } catch (error) {
-      next(error);
-    }
+// Delete (Remove)
+const remove = ({ model, name }) => async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    await model.findByIdAndDelete(id).exec();
+    res.json({
+      success: true,
+      message: `Removed ${name} Instance (${id})`,
+    });
+  } catch (error) {
+    next(error);
   }
+};
 
-  // Delete
-  async delete(req, res, next) {
-    try {
-      console.debug(`Delete ${this.name}`);
-      const id = req.params.id;
-      await this.model.findByIdAndDelete(id).exec();
-      res.json({
-        success: true,
-        message: `Deleted ${this.name} Instance (${id})`,
-      });
-    } catch (error) {
-      next(error);
-    }
+// List (Paginated)
+const list = ({ model, name, collection }) => async (req, res, next) => {
+  try {
+    const { limit = 10, page = 0 } = req.query;
+    const instances = await model
+      .find({})
+      .skip(limit * page)
+      .limit(limit)
+      .exec();
+    const count = await model.count({}).exec();
+    res.json({
+      [collection]: instances,
+      meta: {
+        count,
+        page,
+        pages: Math.ceil(count / limit),
+        limit,
+      },
+    });
+  } catch (error) {
+    next(error);
   }
+};
 
-  // List (Paginated)
-  async list(req, res, next) {
-    try {
-      console.debug(`List ${this.name}`);
-      const { limit = 10, page = 0 } = req.query;
-      const instances = await this.model
-        .find({})
-        .skip(limit * page)
-        .limit(limit)
-        .exec();
-      const count = await this.model.count({}).exec();
-      res.json({
-        [this.collection]: instances,
-        meta: {
-          count,
-          page,
-          pages: Math.ceil(count / limit),
-          limit,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
-}
+const wrappedModel = (model) => ({
+  model,
+  name: model.modelName,
+  collection: model.collection.name,
+});
 
 export function resource(model) {
   const router = new Router();
-  const resource = new ModelController(model);
-  const name = resource.name;
+  const wrapped = wrappedModel(model);
+  const { name } = wrapped;
 
-  // binding is necessary to resolve this in methods
-
-  router.post(`/${name}`, resource.create.bind(resource));
+  router.post(`/${name}`, create(wrapped));
   router
     .route(`/${name}/:id`)
-    .get(resource.read.bind(resource))
-    .put(resource.replace.bind(resource))
-    .patch(resource.modify.bind(resource))
-    .delete(resource.delete.bind(resource));
-  router.get(`/${name}`, resource.list.bind(resource));
+    .get(read(wrapped))
+    .put(replace(wrapped))
+    .patch(modify(wrapped))
+    .delete(remove(wrapped));
+  router.get(`/${name}`, list(wrapped));
 
   return router;
 }
