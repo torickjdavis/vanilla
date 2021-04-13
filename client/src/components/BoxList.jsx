ce475caefad4ce74c82c87d0eeb6b3905d5dad85
Torick J Davis <torickjdavis@gmail.com>
2021-04-13 01:02:15 -0600

    Add Loading States and Use API Helper Routes
    For both Boxes and Recipes.

--- client/src/components/BoxList.jsx
+++ client/src/components/BoxList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { default as RouteLink } from './Link';
 import {
   Grid,
@@ -7,13 +8,19 @@ import {
   ListItemIcon,
   ListItemSecondaryAction,
   ListItemText,
+  Snackbar,
   makeStyles,
 } from '@material-ui/core';
+import { Alert, AlertTitle } from '@material-ui/lab';
 import {
   ChevronRight as ChevronRightIcon,
+  Edit as EditIcon,
   Inbox as BoxIcon,
 } from '@material-ui/icons';
 import { useLocation } from 'react-router-dom';
+import { useBoxes } from '../contexts/BoxContext';
+import useAPI from '../hooks/useAPI';
+import { useAuth } from '../contexts/AuthContext';
 
 const useStyles = makeStyles((theme) => ({
   link: {
@@ -23,41 +30,84 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function BoxList({ boxes }) {
+function BoxListItem({ _id, name, recipes = [], description = '' }) {
+  return (
+    <ListItem button>
+      <ListItemIcon>
+        <BoxIcon />
+      </ListItemIcon>
+      <ListItemText
+        primary={name}
+        secondary={`${recipes.length} Recipes${
+          description ? `: ${description}` : ''
+        }`}
+      />
+      <ListItemSecondaryAction>
+        <IconButton>
+          <ChevronRightIcon />
+        </IconButton>
+        <IconButton
+          onClick={(e) => {
+            e.preventDefault();
+            console.log(`Edit Box (${_id})`);
+          }}
+        >
+          <EditIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+}
+
+export default function BoxList({ userOnly }) {
   const classes = useStyles();
   const location = useLocation();
+  const { user } = useAuth();
+  const allBoxes = useBoxes();
+  const userBoxes = useAPI(`/userBoxes/${user._id}`);
+
+  const [loading, setLoading] = useState(true);
+  const [boxes, setBoxes] = useState([]);
+
+  useEffect(() => {
+    setLoading(userOnly ? userBoxes.loading : allBoxes.loading);
+    setBoxes(userOnly ? userBoxes.data?.boxes : allBoxes.boxes);
+  }, [userBoxes, allBoxes]);
 
   return (
     <Grid container>
       <Grid item xs={12}>
         <List>
-          {boxes?.map(({ _id, name, description, recipes }) => (
-            <RouteLink
-              to={{
-                pathname: `/box/${_id}`,
-                state: { backdrop: location },
-              }}
-              className={classes.link}
-              key={`box-${_id}`}
-            >
-              <ListItem button>
-                <ListItemIcon>
-                  <BoxIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary={name}
-                  secondary={`${recipes.length} Recipes${
-                    description ? `: ${description}` : ''
-                  }`}
+          {loading && (
+            <Snackbar open={loading}>
+              <Alert variant="filled" severity="info">
+                <AlertTitle>Loading Boxes</AlertTitle>
+                This might take a moment.
+              </Alert>
+            </Snackbar>
+          )}
+          {loading &&
+            Array(5)
+              .fill(0)
+              .map((_, index) => <BoxListItem name="Many Delicious Recipes" />)}
+          {!loading &&
+            boxes?.map(({ _id, name, description, recipes }) => (
+              <RouteLink
+                to={{
+                  pathname: `/box/${_id}`,
+                  state: { backdrop: location },
+                }}
+                className={classes.link}
+                key={`box-${_id}`}
+              >
+                <BoxListItem
+                  _id={_id}
+                  name={name}
+                  recipes={recipes}
+                  description={description}
                 />
-                <ListItemSecondaryAction>
-                  <IconButton>
-                    <ChevronRightIcon />
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            </RouteLink>
-          ))}
+              </RouteLink>
+            ))}
         </List>
       </Grid>
     </Grid>
--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
+import { Grid, Snackbar } from '@material-ui/core';
+import { Alert, AlertTitle } from '@material-ui/lab';
 import SkeletonCard from '../components/SkeletonCard';
-import { Grid } from '@material-ui/core';
 import RecipeCard from './RecipeCard';
+import { useAuth } from '../contexts/AuthContext';
+import { useRecipes } from '../contexts/RecipeContext';
+import useAPI from '../hooks/useAPI';
 
-export default function RecipeList({ recipes, users, loading }) {
+export default function RecipeList({ userOnly }) {
+  const { user } = useAuth();
+
+  const allRecipes = useRecipes();
+  const userRecipes = useAPI(`/userRecipes/${user._id}`);
+  const [loading, setLoading] = useState(true);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    setLoading(userOnly ? userRecipes.loading : allRecipes.loading);
+    setRecipes(userOnly ? userRecipes.data?.recipes : allRecipes.recipes);
+  }, [userRecipes, allRecipes]);
+
   return (
     <Grid container spacing={2}>
+      {loading && (
+        <Snackbar open={loading}>
+          <Alert variant="filled" severity="info">
+            <AlertTitle>Loading Recipes</AlertTitle>
+            This might take a moment.
+          </Alert>
+        </Snackbar>
+      )}
       {loading &&
         Array(10)
           .fill(0)
@@ -15,10 +40,9 @@ export default function RecipeList({ recipes, users, loading }) {
           ))}
       {!loading &&
         recipes?.map((recipe) => {
-          const user = users?.find((u) => u._id === recipe.created.by);
           return (
             <Grid item xs={12} md={4} lg={3} key={`recipe-${recipe._id}`}>
-              <RecipeCard recipe={recipe} user={user} />
+              <RecipeCard recipe={recipe} />
             </Grid>
           );
         })}
