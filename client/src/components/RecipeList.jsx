4cae48b1370e821484a2abc03a50fae322c2a52e
Torick J Davis <torickjdavis@gmail.com>
2021-04-19 01:07:15 -0600

    Use Conditional Request and Add Grid Breakpoint

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -7,18 +7,22 @@ import { useAuth } from '../contexts/AuthContext';
 import { useRecipes } from '../contexts/RecipeContext';
 import useAPI from '../hooks/useAPI';
 
+// TODO add error state
+
 export default function RecipeList({ userOnly }) {
   const { user } = useAuth();
 
   const allRecipes = useRecipes();
-  const userRecipes = useAPI(`/userRecipes/${user._id}`);
+  const userRecipes = useAPI(`/userRecipes/${user?._id}`, {
+    skipRequest: !user?._id,
+  });
   const [loading, setLoading] = useState(true);
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     setLoading(userOnly ? userRecipes.loading : allRecipes.loading);
     setRecipes(userOnly ? userRecipes.data?.recipes : allRecipes.recipes);
-  }, [userRecipes, allRecipes]);
+  }, [userRecipes, allRecipes, userOnly]);
 
   return (
     <Grid container spacing={2}>
@@ -34,14 +38,21 @@ export default function RecipeList({ userOnly }) {
         Array(10)
           .fill(0)
           .map((_, index) => (
-            <Grid item xs={12} md={4} lg={3} key={index}>
+            <Grid item xs={12} md={4} lg={3} key={`recipe-temporary-${index}`}>
               <SkeletonCard avatar media />
             </Grid>
           ))}
       {!loading &&
         recipes?.map((recipe) => {
           return (
-            <Grid item xs={12} md={4} lg={3} key={`recipe-${recipe._id}`}>
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              key={`recipe-${recipe._id}`}
+            >
               <RecipeCard recipe={recipe} />
             </Grid>
           );
