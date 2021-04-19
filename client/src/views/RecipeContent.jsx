e3a4e927d29356c1b2275dcd1311df7dc9729a6f
Torick J Davis <torickjdavis@gmail.com>
2021-04-19 01:09:59 -0600

    Simplify RecipeContent

--- client/src/views/RecipeContent.jsx
+++ client/src/views/RecipeContent.jsx
@@ -1,33 +1,12 @@
-import { useRecipes } from '../contexts/RecipeContext';
-import { useUsers } from '../contexts/UserContext';
 import RecipeList from '../components/RecipeList';
-import ErrorInfo from '../components/ErrorInfo';
 import { Typography } from '@material-ui/core';
-import { useAuth } from '../contexts/AuthContext';
-
 export default function RecipeContent({ userOnly = false }) {
-  const {
-    loading: recipesLoading,
-    error: recipesError,
-    recipes,
-  } = useRecipes();
-  const { loading: usersLoading, error: usersError, users } = useUsers();
-  const { user } = useAuth();
-  const filter = (r) => (userOnly ? r.created.by === user?._id : true);
-
-  // prettier-ignore
-  if (recipesError || usersError) return <ErrorInfo error={recipesError || usersError} />;
-
   return (
     <>
       <Typography variant="h2" paragraph>
         Recipes
       </Typography>
-      <RecipeList
-        recipes={recipes?.filter(filter)}
-        users={users}
-        loading={recipesLoading || usersLoading}
-      />
+      <RecipeList userOnly={userOnly} />
     </>
   );
 }
