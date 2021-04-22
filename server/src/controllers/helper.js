2cf1257d77219e76f8a2dc268e58771a385432b9
Torick J Davis <torickjdavis@gmail.com>
2021-04-22 14:51:02 -0600

    Add Token and Password Verification to User
    Additionally, created refactored login controller in helpers to be used.

--- server/src/controllers/helper.js
+++ server/src/controllers/helper.js
@@ -3,6 +3,27 @@ import User from '../models/user.js';
 import Recipe from '../models/recipe.js';
 import Box from '../models/box.js';
 
+export async function login(req, res) {
+  try {
+    const validPassword = await User.verifyPassword(
+      req.body.email,
+      req.body.password
+    );
+
+    if (!validPassword) return res.sendStatus(status.FORBIDDEN);
+
+    const user = await User.findOne({ email: req.body.email }).exec();
+    const accessToken = await user.generateToken();
+
+    res.json({ accessToken, user }); // user details in signed token
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(status.NOT_FOUND)
+      .json({ message: `No ${User.modelName} Found` });
+  }
+}
+
 export async function userRecipes(req, res, next) {
   try {
     const userId = req.params.userId;
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import Box from './box.js';
 import Recipe from './recipe.js';
+
+const SALT_ROUNDS = 10;
+const { JWT_SECRET } = process.env;
 
 export const UserSchema = new mongoose.Schema({
   email: {
@@ -11,6 +16,7 @@ export const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
   name: {
     first: String,
@@ -23,6 +29,34 @@ UserSchema.virtual('name.full').get(function () {
   return [this.name.first || null, this.name.last || null]
     .filter((v) => !!v)
     .join(' ');
+});
+
+UserSchema.pre('validate', async function () {
+  if (this.password && this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  }
+});
+
+UserSchema.statics.verifyPassword = async function (email, plainTextPassword) {
+  const user = await this.findOne({ email }).select('+password').exec();
+  if (!user) throw new Error('Not Found');
+  return await bcrypt.compare(plainTextPassword, user.password);
+};
+
+UserSchema.methods.generateToken = async function () {
+  return jwt.sign(this.toJSON(), JWT_SECRET, {
+    expiresIn: '1d',
+  });
+};
+
+UserSchema.static('verifyToken', async function (token) {
+  const User = this;
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, JWT_SECRET, (error, user) => {
+      if (error) reject(error);
+      resolve(User.hydrate(user));
+    });
+  });
 });
 
 UserSchema.post('remove', async function () {
